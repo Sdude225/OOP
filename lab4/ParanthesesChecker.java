9b56902050927c486e63c1424338b90d42a3890b
Valera <pavlov.alexandru@gmail.com>
2019-11-06 13:34:24 +0200

    lab4 v3

--- lab4/ParanthesesChecker.java
+++ lab4/ParanthesesChecker.java
@@ -6,6 +6,7 @@ public class ParanthesesChecker {
     public Stack<Character> parentheses = new Stack<Character>();
     public String[] temp;
     public List<String> lines = new ArrayList<String>();
+    boolean isnotValid = true;
 
     public ParanthesesChecker(String Text){
         temp = Text.split("([\\r?\\n]+)([\\r?\\n' ']+)");
@@ -18,16 +19,22 @@ public class ParanthesesChecker {
 
     public void isValid(){
         for(int i = 0; i < lines.size(); i++){
+            isnotValid = true;
             for(int j = 0; j < lines.get(i).length(); j++){
                 if(lines.get(i).charAt(j) == '('){ //lines[i].charAt(j)
                     parentheses.push('(');
                 }
                 else if(lines.get(i).charAt(j) == ')'){
                     if(parentheses.isEmpty()){
-                        continue;
+                        System.out.println("Line " + (i+1) + " is not valid");
+                        isnotValid = false;
+                        break;
                     }
                     parentheses.pop();
                 }
+            }
+            if(!isnotValid){
+                continue;
             }
             if(parentheses.isEmpty()){
                 System.out.println("Line " + (i+1) + " is valid");
